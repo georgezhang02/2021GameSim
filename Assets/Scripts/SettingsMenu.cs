f62dd186ff6fe0ab84aba1a3d26930d0be386783
George Zhang <georgezhang02@hotmail.com>
2021-03-21 20:55:26 -0700

    Resolution saved

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -6,28 +6,40 @@ using TMPro;
 public class SettingsMenu : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown;
-
     List<string> options;
 
     void Start()
     {
-        // Create options dropdown and select current resolution
         Resolution[] resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         options = new List<string>();
 
+        // Find current resolution
+        int width = Screen.currentResolution.width;
+        int height = Screen.currentResolution.height;
+
+        int currentIndex = -1;
+
+        // Create options dropdown and select current resolution
+        int j = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
+
             string option = resolutions[i].width + " x " + resolutions[i].height;
             if (!options.Contains(option) && resolutions[i].width >= 1280)
             {
+                if (resolutions[i].width == width && resolutions[i].height == height)
+                {
+                    currentIndex = j;
+                }
+
                 options.Add(option);
+                j++;
             }
         }
 
         resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = options.Count - 1;
-        SetResolution(options.Count - 1);
+        resolutionDropdown.value = currentIndex;
         resolutionDropdown.RefreshShownValue();
     }
 
